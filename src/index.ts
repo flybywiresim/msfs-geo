d0998d6709a928e9432ce47b1c683cdf3824f4e2
Alex Cutforth <alex@densem.net>
2022-01-18 13:37:35 +1300

    feat: rollup and npm publish workflow

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './bearingTo';
+export * from './common';
+export * from './constants';
+export * from './distanceTo';
+export * from './greatCircleIntersection';
+export * from './placeBearingDistance';
+export * from './trig';
