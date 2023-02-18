c60e70c2d2952e15b79915f46a64548a2f785d72
Michael Corcoran <tracer@outlook.co.nz>
2023-02-19 00:21:56 +1300

    feat: overloads for distanceTo

--- src/distanceTo.spec.ts
+++ src/distanceTo.spec.ts
@@ -7,14 +7,22 @@ describe('distanceTo', () => {
                 { lat: 52.518611, long: 13.408056 },
                 { lat: 51.519475, long: 7.46694444 },
             ),
-        ).toEqual(227.74647067942598);
+        ).toBeCloseTo(227.74647067942598);
+
+        expect(
+            distanceTo(52.518611, 13.408056, 51.519475, 7.46694444),
+        ).toBeCloseTo(227.74647067942598);
 
         expect(
             distanceTo(
                 { lat: 37.774514, long: -122.418079 },
                 { lat: 51.519475, long: 7.46694444 },
             ),
-        ).toEqual(4841.885420848646);
+        ).toBeCloseTo(4841.885420848646);
+
+        expect(
+            distanceTo(37.774514, -122.418079, 51.519475, 7.46694444),
+        ).toBeCloseTo(4841.885420848646);
     });
 
     it('should return 0 if two identical points are given', () => {
@@ -23,13 +31,14 @@ describe('distanceTo', () => {
                 { lat: 51.516241843, long: 7.456494328 },
                 { lat: 51.516241843, long: 7.456494328 },
             ),
-        ).toBe(0);
+        ).toBeCloseTo(0);
 
         expect(
-            distanceTo(
-                { lat: 51.516241842, long: 7.456494328 },
-                { lat: 51.516241842, long: 7.456494328 },
-            ),
-        ).toBe(0);
+            distanceTo(51.516241843, 7.456494328, 51.516241843, 7.456494328),
+        ).toBeCloseTo(0);
+
+        expect(
+            distanceTo(51.516241842, 7.456494328, 51.516241842, 7.456494328),
+        ).toBeCloseTo(0);
     });
 });
